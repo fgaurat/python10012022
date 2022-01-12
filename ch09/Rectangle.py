217c9fb8d0786b9ef6941f987831e6906abac303
Frédéric Gaurat <fgaurat@eolem.com>
2022-01-12 16:27:21 +0100

    python obj rectangle

--- ch09/Rectangle.py
+++ ch09/Rectangle.py
@@ -5,5 +5,18 @@ class Rectangle:
     
     def __init__(self,longueur,largeur) -> None:
         print(f"def __init__(self,{longueur=},{largeur=})")
-        self.longueur = longueur
-        self.largeur = largeur
+        self._longueur = longueur
+        self._largeur = largeur
+
+    def get_longueur(self):
+        return self._longueur
+
+    def get_largeur(self):
+        return self._largeur
+    
+    def set_longueur(self,longueur):
+        self._longueur = longueur
+
+    def set_largeur(self,largeur):
+        self._largeur = largeur
+    
--- ch09/main.py
+++ ch09/main.py
@@ -3,12 +3,18 @@ from Rectangle import Rectangle
 
 def main():
     r = Rectangle(12,5) # Créer un objet r instance de la class Rectangle
-    
+
     longueur = r.get_longueur()
     print(longueur) # 12
     
     largeur = r.get_largeur()
     print(largeur) # 5
+
+    r.set_longueur(2)
+    r.set_largeur(4)
+
+    print(r.get_longueur())
+    print(r.get_largeur())
 
 if __name__ == '__main__':
     main()
