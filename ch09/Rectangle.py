5ab3972870ceb50263a400257974955ac06371da
Frédéric Gaurat <fgaurat@eolem.com>
2022-01-12 15:41:54 +0100

    python obj rectangle

---
+++ ch09/Rectangle.py
@@ -0,0 +1,9 @@
+
+
+
+class Rectangle:
+    
+    def __init__(self,longueur,largeur) -> None:
+        print(f"def __init__(self,{longueur=},{largeur=})")
+        self.longueur = longueur
+        self.largeur = largeur
