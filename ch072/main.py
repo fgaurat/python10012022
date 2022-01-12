5fa8898822365f2c45152db9b4a478bf93063b5c
Frédéric Gaurat <fgaurat@eolem.com>
2022-01-12 11:18:48 +0100

    io

---
+++ ch072/main.py
@@ -0,0 +1,28 @@
+
+
+def main():
+    with open(r"lefichier.txt","w") as f:
+        # f.write("Toto\n")
+        # f.write("Titi\n")
+        print("Toto",file=f)
+        print("Titi",file=f)
+        print("Toto",file=f)
+        print("Titi",file=f)
+    
+    with open(r"lefichier.txt","r") as f:
+        # Lit la totalité dans une chaîne
+        # data = f.read()
+        # print(data)
+        data =f.readlines()
+        print(data)
+        for n,line in enumerate(data):
+            s = f"ligne {n+1} {line.strip()}"
+            print(s)
+
+        # ligne 1 : Toto
+        # ligne 2 : Titi
+    
+
+
+if __name__ == '__main__':
+    main()
