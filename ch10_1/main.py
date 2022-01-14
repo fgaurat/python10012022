483cafd7bff17dbd9875cd33af261bcb3aa433bc
Frédéric Gaurat <fgaurat@eolem.com>
2022-01-14 10:04:17 +0100

    tp ch10_1

--- ch10_1/main.py
+++ ch10_1/main.py
@@ -1,5 +1,10 @@
+import json
+
 def main():
-    pass
+    with open('todos.json') as f:
+        data = json.load(f)
+        todo = data[0] 
+        print(todo['title'])
 
 if __name__ == '__main__':
     main()
